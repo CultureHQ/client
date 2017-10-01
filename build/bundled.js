07506793de15dcd244e16129c034ff79314c8627
Kevin Deisz <kevin.deisz@gmail.com>
2017-10-01 15:40:53 -0400

    Update builds

--- build/bundled.js
+++ build/bundled.js
@@ -20496,11 +20496,12 @@ var snakerizeString = function snakerizeString(string) {
 };
 
 var modifyKeys = function modifyKeys(object, stringFunc) {
+  if ((typeof object === "undefined" ? "undefined" : _typeof(object)) !== "object" || object === null) {
+    return object;
+  }
+
   var modified = {};
   var value = void 0;
-
-  // If object is undefined/null
-  if (typeof object == 'undefined' || object == null) return;
 
   Object.keys(object).forEach(function (key) {
     value = object[key];
