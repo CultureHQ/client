b6fff91de7a7a0cc614889736d5db630e199e6a7
Kevin Deisz <kevin.deisz@gmail.com>
2017-11-01 21:17:36 -0400

    Fix unexpected end of json

--- src/request.js
+++ src/request.js
@@ -46,23 +46,27 @@ const buildRequest = (method, url, options) => {
 export default (method, url, options) => {
   const request = buildRequest(method, url, options);
 
-  return new Promise((resolve, reject) => {
-    fetch(request.url, request.options)
-      .then(response => {
-        if (Math.round(response.status / 200) !== 1) {
-          response
-            .json()
-            .then(json => reject(camelize(json)))
-            .catch(error => reject(error));
-        } else if (response.status === 204) {
-          resolve(null);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(request.url, request.options);
+      const success = Math.round(response.status / 200) === 1;
+
+      if (response.status === 204) {
+        resolve(null);
+      } else if (response.headers.get("content-type") === "text/html") {
+        if (success) {
+          const text = await response.text();
+          resolve({ response, text });
         } else {
-          response
-            .json()
-            .then(json => resolve(camelize(json)))
-            .catch(error => reject(error));
+          reject({ response, error: response.statusText });
         }
-      })
-      .catch(error => reject(error));
+      } else {
+        const json = await response.json();
+        const fullResponse = Object.assign({ response }, camelize(json));
+        success ? resolve(fullResponse) : reject(fullResponse);
+      }
+    } catch (error) {
+      reject(error);
+    }
   });
 };
