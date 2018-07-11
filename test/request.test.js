9f80c09580ceefdf8267614bcf4aeee80e65dcc2
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-11 07:53:44 -0400

    More test coverage for requests

--- test/request.test.js
+++ test/request.test.js
@@ -18,6 +18,36 @@ const parseMultipart = request => {
   return values;
 };
 
+test("attaches GET params directly to the query string", async () => {
+  const server = createServer({ status: 200, body: {} });
+  server.listen(1693);
+
+  try {
+    const { response } = await request("GET", new URL("http://localhost:1693"), {
+      params: { one: "one", two: [], three: [1, 2, 3], four: null, five: undefined }
+    });
+
+    const entries = new URL(response.url).searchParams.entries();
+    const expected = [
+      ["one", "one"],
+      ["three[]", "1"],
+      ["three[]", "2"],
+      ["three[]", "3"]
+    ];
+
+    let next;
+    for (next = entries.next(); !next.done; next = entries.next()) {
+      const { value: [actualKey, actualValue] } = next;
+      const [expectedKey, expectedValue] = expected.shift();
+
+      expect(actualKey).toEqual(expectedKey);
+      expect(actualValue).toEqual(expectedValue);
+    }
+  } finally {
+    server.close();
+  }
+});
+
 test("passes nulls through as empty strings on multipart", async () => {
   const server = createServer({ status: 200, body: { foo: "bar" } });
   const port = 1694;
