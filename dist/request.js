595dcf74386f93412d559ad340773cca60f5676e
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-26 09:09:31 -0400

    Bump to 2.2.0

--- dist/request.js
+++ dist/request.js
@@ -21,7 +21,7 @@ var buildHeaders = function buildHeaders(_ref) {
       token = _ref.token,
       simulation = _ref.simulation;
 
-  var headers = { "X-Client-Version": "2.1.2" };
+  var headers = { "X-Client-Version": "2.2.0" };
 
   if (!multipart) {
     headers["Content-Type"] = "application/json";
