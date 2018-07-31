e94bfee5e986e68739b23ee50577f3a8f18ed957
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-31 15:41:54 -0400

    Better exports for constants

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,25 @@
-const PROD = {
-  API_HOST: "https://api.culturehq.com",
-  AWS_ACCESS_KEY_ID: "AKIAIPWT257FWZ5I4ZGQ",
-  SIGNER_URL: "https://726l7tpb5a.execute-api.us-west-2.amazonaws.com/production/signature",
-  UPLOAD_BUCKET: "https://culturehq-direct-uploads.s3-us-west-2.amazonaws.com"
+const constants = {
+  production: {
+    API_HOST: "https://api.culturehq.com",
+    AWS_ACCESS_KEY_ID: "AKIAIPWT257FWZ5I4ZGQ",
+    SIGNER_URL: "https://726l7tpb5a.execute-api.us-west-2.amazonaws.com/production/signature",
+    UPLOAD_BUCKET: "https://culturehq-direct-uploads.s3-us-west-2.amazonaws.com"
+  },
+  development: {
+    API_HOST: "http://localhost:3000",
+    AWS_ACCESS_KEY_ID: null,
+    SIGNER_URL: "http://localhost:3001",
+    UPLOAD_BUCKET: "http://localhost:3001"
+  },
+  test: {
+    API_HOST: "http://localhost:8080",
+    AWS_ACCESS_KEY_ID: "access-key-id",
+    SIGNER_URL: "http://localhost:8081",
+    UPLOAD_BUCKET: "http://localhost:8082"
+  }
 };
 
-const DEV = {
-  API_HOST: "http://localhost:3000",
-  AWS_ACCESS_KEY_ID: null,
-  SIGNER_URL: "http://localhost:3001",
-  UPLOAD_BUCKET: "http://localhost:3001"
-};
+const { API_HOST, AWS_ACCESS_KEY_ID, SIGNER_URL, UPLOAD_BUCKET } =
+  constants[process.env.NODE_ENV || "development"];
 
-export process.env.NODE_ENV === "production" ? PROD : DEV;
+export { API_HOST, AWS_ACCESS_KEY_ID, SIGNER_URL, UPLOAD_BUCKET };
