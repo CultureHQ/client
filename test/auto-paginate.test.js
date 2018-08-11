67108883ab079d7b1faeb1bb4775d61e4f6f73ea
Kevin Deisz <kevin.deisz@gmail.com>
2018-08-11 12:13:31 -0400

    Better default export from auto-paginate

--- test/auto-paginate.test.js
+++ test/auto-paginate.test.js
@@ -1,5 +1,5 @@
-import AutoPaginator from "../src/auto-paginator";
-import API_CALLS from "../src/api-calls";
+import apiCalls from "../src/api-calls";
+import autoPaginate from "../src/auto-paginate";
 
 jest.mock("../src/api-calls", () => ({ listEvents: jest.fn() }));
 
@@ -7,19 +7,19 @@ const PER_PAGE = 5;
 const EVENTS = [...Array(18)].map((empty, idx) => ({ idx, name: `Event ${idx}` }));
 
 test("exits early if there's only one page of responses", async () => {
-  API_CALLS.listEvents.mockImplementation(({ page }) => Promise.resolve({
+  apiCalls.listEvents.mockImplementation(({ page }) => Promise.resolve({
     events: EVENTS,
     pagination: { currentPage: page, totalPages: 1, totalCount: EVENTS.length }
   }));
 
-  const { events, pagination } = await new AutoPaginator("events").listEvents();
+  const { events, pagination } = await autoPaginate("events").listEvents();
 
   expect(events.length).toEqual(EVENTS.length);
   expect(pagination.totalPages).toEqual(1);
 });
 
 test("concatenates all results together", async () => {
-  API_CALLS.listEvents.mockImplementation(({ page }) => {
+  apiCalls.listEvents.mockImplementation(({ page }) => {
     const events = EVENTS.slice((page - 1) * PER_PAGE, page * PER_PAGE);
     const pagination = {
       currentPage: page,
@@ -30,7 +30,7 @@ test("concatenates all results together", async () => {
     return Promise.resolve({ events, pagination });
   });
 
-  const { events, pagination } = await new AutoPaginator("events").listEvents();
+  const { events, pagination } = await autoPaginate("events").listEvents();
 
   expect(events.length).toEqual(EVENTS.length);
   expect(pagination.totalPages).toEqual(Math.ceil(EVENTS.length / PER_PAGE));
