602273996bbe964626730d3d47d7c14241681d49
Kevin Deisz <kevin.deisz@gmail.com>
2018-11-13 08:55:40 -0500

    Bump to 6.5.0

--- dist/request.js
+++ dist/request.js
@@ -17,7 +17,7 @@ var buildHeaders = function buildHeaders(_ref) {
   var token = _ref.token,
       simulation = _ref.simulation;
   var headers = {
-    "X-Client-Version": "6.4.1",
+    "X-Client-Version": "6.5.0",
     "Content-Type": "application/json"
   };
 
