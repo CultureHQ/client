79d10d1c831b567642bf7d169ff5f6a9af0765f3
rodriguezjuan6258 <juan@culturehq.com>
2024-12-18 08:15:00 -0600

    fix: change prod upload signer url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   apiHost: "https://api.culturehq.com",
   awsAccessKeyId: "AKIAIPWT257FWZ5I4ZGQ",
-  signerURL: "https://fyzqa1okfe.execute-api.us-west-2.amazonaws.com/production/signature",
+  signerURL: "https://production-upload-signer.culturehq.com/production/signature",
+  signerAuthToken: "SIGNER-AUTH-TOKEN",
   uploadBucket: "https://culturehq-direct-uploads.s3-us-west-2.amazonaws.com",
   AWSAccessKey: "AWS-KEY",
   AWSSecretAccessKey: "AWS-SECRET"
